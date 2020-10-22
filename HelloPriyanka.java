9771f99e252d59686a7a5cf4c6937036fe6bda4b
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-22 14:26:29 +0530

    first Commit

---
+++ HelloPriyanka.java
@@ -0,0 +1,9 @@
+public class HelloPriyanka {
+
+	public static void main(String[] args) {
+		
+               System.out.println("Hello World");
+               System.out.println("Priyanka karkar");
+	}
+
+}
