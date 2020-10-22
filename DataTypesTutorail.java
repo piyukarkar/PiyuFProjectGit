9e66ac60d66061bfb353d581544245f59d941dc1
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-22 15:01:40 +0530

    Data types

---
+++ DataTypesTutorail.java
@@ -0,0 +1,22 @@
+public class DataTypesTutorial {
+
+	public static void main(String[] args) {
+
+		int age = 10;
+		
+		float rateOfInterest =2.3f;
+		
+		double rate = 3.14258;
+		
+		System.out.println(age);
+		
+		boolean isThisSeriesCool = true;
+		
+		char myCharacter = '@';
+
+        System.out.println(myCharacter);
+        
+
+	}
+
+}
